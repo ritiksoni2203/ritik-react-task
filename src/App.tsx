8b3fae8f40ee4dfeb750a9d917d6105f24acbd03
parthv2023 <parth@creativegeeks.online>
2023-07-07 17:40:52 +0530

    routes handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import './App.css'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useNavigate } from 'react-router-dom'
 import Home from './pages/home'
 import Profile from './pages/home/Profile'
 import Login from './pages/login'
 import Callback from './pages/callback'
 import ProtectedRoute from './components/routes/ProtectedRoute'
 import NotFound from './pages/notfound'
+import { useEffect } from 'react'
 
 function App() {
+  const navigate = useNavigate();
+  const token = localStorage.getItem('access') ? true : false;
+
+  useEffect(() => {
+    !token ? navigate('/login') : navigate('/repos')
+  }, []);
 
   return (
     <Routes>
